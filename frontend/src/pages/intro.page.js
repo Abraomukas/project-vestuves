2fb191d47e77a39f7ee2ee7410a16c8ce9ec8926
Abraomukas <abraomukas@gmail.com>
2022-01-09 19:20:13 +0400

    feat: page stubs added

---
+++ frontend/src/pages/intro.page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function IntroPage() {
+	return (
+		<div>
+			<div>
+				<h1>INTRO</h1>
+			</div>
+		</div>
+	);
+}
+
+export default IntroPage;
---
+++ frontend/src/pages/location.page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LocationPage() {
+	return (
+		<div>
+			<div>
+				<h1>LOCATION</h1>
+			</div>
+		</div>
+	);
+}
+
+export default LocationPage;
